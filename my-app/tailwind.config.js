cf5581f433fe32aee462052e9242e83deec232fa
SunitaKumari <172397566+sunitakumari22@users.noreply.github.com>
2025-04-25 12:48:17 +0530

    updated

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('nativewind/preset')],
-  content: [
-    "./App.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
