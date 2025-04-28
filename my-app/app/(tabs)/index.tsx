253c63003fc8f726e0d50e9793b7417d7fb1bc99
SunitaKumari <172397566+sunitakumari22@users.noreply.github.com>
2025-04-28 11:47:31 +0530

    Added layout ,index,profile,saved and search component

---
+++ my-app/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import "../global.css"
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className=" flex-1 justify-center items-center text-center">
+      
+      <Text className="text-center text-5xl text-dark-500 font-bold">Welcome Siyaaa!</Text>
+       <Link href="/onboarding">onboarding</Link>
+       <Link href={{ pathname: "/movie/[id]", params: { id: "avangers" } }}> Avangers Movie</Link>
+
+    </View>
+  );
+}
